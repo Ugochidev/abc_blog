04b44f63282e67331d114e2cbc70ba17ba7a0cf3
Ugochidev <ugochukwuchioma16@gmail.com>
2023-08-09 08:24:07 +0100

    first commit

---
+++ src/DBConnect/connectPGsql.js
@@ -0,0 +1,23 @@
+const { Pool } = require("pg");
+const dotenv = require("dotenv");
+
+dotenv.config();
+
+// creating connection:
+const pool = new Pool({
+  host: process.env.hostpg,
+  user: process.env.userpg,
+  port: process.env.portpg,
+  password: process.env.passwordpg,
+  database: process.env.databasepg,
+}); 
+
+pool.query("SELECT NOW()", (err, res) => {
+  if (!err) {
+    console.log("connected to PGsql");
+  } else {
+    console.log(err.message);
+  }
+});
+
+module.exports = pool;
---
+++ src/model/usermodel.js
@@ -0,0 +1,27 @@
+// // -- DATABASE --
+// CREATE DATABASE abc_blog;
+
+// -- USER TABLE --
+// CREATE TABLE users
+// (
+// 	id VARCHAR UNIQUE PRIMARY KEY,
+// 	first_name VARCHAR(50),
+// 	last_name VARCHAR(50),
+// 	email VARCHAR(100),
+// 	phone_number VARCHAR(15),
+// 	password VARCHAR(100)
+// );
+
+
+class AbcBlog {
+  constructor() {
+    this.id = id;
+    this.first_name = first_name;
+    this.last_name = last_name;
+    this.email = email;
+    this.phone_number = phone_number;
+    this.password = password;
+  }
+}
+
+module.exports = AbcBlog;
